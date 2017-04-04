c1ec91e42f2eae4930029bfa8eef3d06e6ebc6f6
Sean-Peterson <seanpeterson11@gmail.com>
2017-04-04 16:33:30 -0700

    Delete api-keys.ts

--- src/app/api-keys.ts
+++
@@ -1,7 +0,0 @@
-export const masterFirebaseConfig = {
-  apiKey: "AIzaSyBvQblKLFmQ3LvF56s-urlZ9SfdQOk95sI",
-    authDomain: "dreamboard-b5976.firebaseapp.com",
-    databaseURL: "https://dreamboard-b5976.firebaseio.com",
-    storageBucket: "dreamboard-b5976.appspot.com",
-    messagingSenderId: "986996541301"
-  };
